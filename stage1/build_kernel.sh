e2c96b741e4d9d355cb2e73307dca1f830819130
UMRnInside <newpage2@foxmail.com>
2018-01-19 19:00:19 +0800

    stage1: prepare and build kernel are separated

--- stage1/build_kernel.sh
+++ stage1/build_kernel.sh
@@ -2,39 +2,10 @@
 
 . $(dirname $0)/../global_definitions
 
-mkdir -p $BUILD_PATH
-mkdir -p $INSTALL_PATH
-
-pushd $BUILD_PATH
-
-CHECKOUT_DEST="rpi-4.12.y"
-if [ ! $SKIP_KERNELFETCH ]; then
-    if [ $FETCH_METHOD == "git" ]; then
-        echo "Fetching kernel using git clone, target $CHECKOUT_DEST ..."
-        git clone $GITCLONE_ARGS ${GIT_PROTOCOL}github.com/raspberrypi/linux -b $CHECKOUT_DEST
-    elif [ $FETCH_METHOD == "wget" ]; then
-        echo "Fetching kernel using wget, target $CHECKOUT_DEST ..."
-        wget -c https://github.com/raspberrypi/linux/archive/${CHECKOUT_DEST}.zip
-        unzip $CHECKOUT_DEST.zip
-        mv linux-${CHECKOUT_DEST} linux
-    fi
-else
-    echo "Skipping kernel fetch, as SKIP_KERNELFETCH is set"
-fi
-
-# Prepare for building
-
-echo "Patching $THERMAL_PATCH_DEST"
-echo "See https://github.com/raspberrypi/linux/issues/2136 for more infomation"
-patch $THERMAL_PATCH_DEST $THERMAL_PATCH_FILE
-
-echo "Copying config..."
-cp $BCMRPI3_CONFFILE $BUILD_PATH/linux/.config
-
 # Environmental configs
 export ARCH CROSS_COMPILE INSTALL_PATH INSTALL_MOD_PATH INSTALL_HDR_PATH
 # Ready to compile
-pushd linux
+pushd $BUILD_PATH/linux
 
 # Compile!
 echo "Compiling kernel, which might take a LONG while..."
---
+++ stage1/prepare_kernel.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. $(dirname $0)/../global_definitions
+
+mkdir -p $BUILD_PATH
+mkdir -p $INSTALL_PATH
+
+pushd $BUILD_PATH
+
+CHECKOUT_DEST="rpi-4.12.y"
+if [ ! $SKIP_KERNELFETCH ]; then
+    if [ $FETCH_METHOD == "git" ]; then
+        echo "Fetching kernel using git clone, target $CHECKOUT_DEST ..."
+        git clone $GITCLONE_ARGS ${GIT_PROTOCOL}github.com/raspberrypi/linux -b $CHECKOUT_DEST
+    elif [ $FETCH_METHOD == "wget" ]; then
+        echo "Fetching kernel using wget, target $CHECKOUT_DEST ..."
+        wget -c https://github.com/raspberrypi/linux/archive/${CHECKOUT_DEST}.zip
+        unzip $CHECKOUT_DEST.zip
+        mv linux-${CHECKOUT_DEST} linux
+    fi
+else
+    echo "Skipping kernel fetch, as SKIP_KERNELFETCH is set"
+fi
+
+# Prepare for building
+
+echo "Patching $THERMAL_PATCH_DEST"
+echo "See https://github.com/raspberrypi/linux/issues/2136 for more infomation"
+patch $THERMAL_PATCH_DEST $THERMAL_PATCH_FILE
+
+echo "Copying config..."
+cp $BCMRPI3_CONFFILE $BUILD_PATH/linux/.config
+
+popd
