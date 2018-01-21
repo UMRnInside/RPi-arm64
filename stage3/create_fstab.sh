8ca895168a133b7f1a56acfa2aa867ce360d4e36
UMRnInside <newpage2@foxmail.com>
2018-01-21 12:45:05 +0800

    Add stage3/create_fstab.sh

---
+++ stage3/create_fstab.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. $(dirname $0)/../global_definitions
+
+if [ ! $ROOT_PATH ]; then
+    echo "WARNING: ROOT_PATH is not set!";
+fi
+
+echo "${BOOT_PART-/dev/mmcblk0p1} /boot   vfat    noauto,noatime  0   0" > $ROOT_PATH/etc/fstab
+
+case ${FSTYPE=f2fs} in
+    ext4|f2fs)
+        echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        ;;
+    ext3|ext2)
+        echo "Rootfs using $FSTYPE, but ext4/f2fs should be better"
+        echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        ;;
+    *)
+        echo "Rootfs using $FSTYPE, make sure it is a valid value."
+        echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    noatime  0   1" >> $ROOT_PATH/etc/fstab
+        ;;
+esac
+
+echo "Done."
