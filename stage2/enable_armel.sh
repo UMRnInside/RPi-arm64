95c51391442ef8ad450d8ba82d407a7b1706070a
UMRnInside <newpage2@foxmail.com>
2018-01-21 15:31:57 +0800

    stage2: fix enable_arm* error

--- stage2/enable_armel.sh
+++ stage2/enable_armel.sh
@@ -3,6 +3,6 @@
 . $(dirname $0)/../global_definitions
 
 echo "Adding armel support"
-chroot $ROOT_PATH dpkg --add-architecure armel
+chroot $ROOT_PATH dpkg --add-architecture armel
 chroot $ROOT_PATH apt-get update
 chroot $ROOT_PATH apt-get -y install libc6:armel
--- stage2/enable_armhf.sh
+++ stage2/enable_armhf.sh
@@ -3,7 +3,7 @@
 . $(dirname $0)/../global_definitions
 
 echo "Adding armhf support"
-chroot $ROOT_PATH dpkg --add-architecure armhf
+chroot $ROOT_PATH dpkg --add-architecture armhf
 chroot $ROOT_PATH apt-get update
 chroot $ROOT_PATH apt-get -y install libc6:armhf
 
