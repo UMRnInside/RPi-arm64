3e22c9615c6e82850134545d34c5569881f46a88
UMRnInside <newpage2@foxmail.com>
2018-01-24 22:31:39 +0800

    stage2: Flexible FWURL for install_firmware_brcm.sh

--- stage2/install_firmware_brcm.sh
+++ stage2/install_firmware_brcm.sh
@@ -2,11 +2,10 @@
 
 . $(dirname $0)/../global_definitions
 
-FWURL="https://archive.raspberrypi.org/debian/pool/main/f/firmware-nonfree/firmware-brcm80211_20161130-3+rpi2_all.deb"
 fileName=firmware-brcm80211.deb
 echo "Retriving firmware..."
 
-wget -c -O $ROOT_PATH/$fileName $FWURL
+wget -c -O $ROOT_PATH/$fileName ${FWURL-"https://archive.raspberrypi.org/debian/pool/main/f/firmware-nonfree/firmware-brcm80211_20161130-3+rpi2_all.deb"}
 echo "Installing firmwares..."
 #firmwares="firmware-brcm80211 "
 
