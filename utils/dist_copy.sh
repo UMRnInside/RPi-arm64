a694308d79fad90499b2cd83822daad1be8703f5
UMRnInside <newpage2@foxmail.com>
2018-01-25 23:00:47 +0800

    Add utils/dist_copy.sh

---
+++ utils/dist_copy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. $(dirname $0)/../global_definitions
+
+function rcopy()
+{
+    if [ -e $(which pv) ]; then
+        tar -p -c -C $1 | pv -bt | tar -p -x -C $2
+    else
+        tar -p -c -C $1 | tar -p -x -C $2
+    fi
+}
+
+if [ $BOOT_PATH ] && [ $BOOT_DEST ]; then
+    echo "Copying boot from $BOOT_PATH to $BOOT_DEST"
+    rcopy $BOOT_PATH $BOOT_DEST
+fi
+
+if [ $ROOT_PATH ] && [ $ROOT_DEST ]; then
+    echo "Copying rootfs from $ROOT_PATH to $ROOT_DEST"
+    rcopy $ROOT_PATH $ROOT_DEST
+fi
