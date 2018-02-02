e08ccb57b16cefe926c38d1a2472ccd72f57d57d
UMRnInside <newpage2@foxmail.com>
2018-02-02 17:23:50 +0800

    utils: install wireless drivers&firmwares

---
+++ utils/install_firmware_wl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. $(dirname $0)/../global_definitions
+
+FIRMWARE="firmware-atheros firmware-iwlwifi firmware-realtek "
+
+echo "Installing firmwares..."
+echo "Attempt to install the following firmwares:"
+echo ${FIRMWARE}${FIRMWARE_EXTRA}
+
+sleep 3
+
+chroot $ROOT_PATH apt-get -y install ${FIRMWARE}${FIRMWARE_EXTRA}
+
