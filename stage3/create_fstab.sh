7645e1090c6cbed8907b99e6a8be2744be61a6c2
UMRnInside <newpage2@foxmail.com>
2018-02-05 17:00:56 +0800

    stage3: btrfs optimize for ssd

--- stage3/create_fstab.sh
+++ stage3/create_fstab.sh
@@ -16,6 +16,9 @@ case $FSTYPE in
         echo "Rootfs using $FSTYPE, but ext4/f2fs should be better"
         echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
         ;;
+    btrfs) 
+        echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    ssd,discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        ;;
     *)
         echo "Rootfs using $FSTYPE, make sure it is a valid value."
         echo "${ROOT_PART-/dev/mmcblk0p2} /   $FSTYPE    noatime  0   1" >> $ROOT_PATH/etc/fstab
