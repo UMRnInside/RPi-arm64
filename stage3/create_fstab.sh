6e3d6595aea05a3bdf3dc7baf43abb1c5c8b64bd
UMRnInside <newpage2@foxmail.com>
2018-02-22 19:54:47 +0800

    Disable discard
    Most SD cards do not support discard/TRIM

--- stage3/create_fstab.sh
+++ stage3/create_fstab.sh
@@ -12,14 +12,14 @@ ROOT_PART=${ROOT_PART-"/dev/mmcblk0p2"}
 
 case $FSTYPE in
     ext4|f2fs)
-        echo "$ROOT_PART /   $FSTYPE    discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        echo "$ROOT_PART /   $FSTYPE    noatime  0   1" >> $ROOT_PATH/etc/fstab
         ;;
     ext3|ext2)
         echo "Rootfs using $FSTYPE, but ext4/f2fs should be better"
-        echo "$ROOT_PART /   $FSTYPE    discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        echo "$ROOT_PART /   $FSTYPE    noatime  0   1" >> $ROOT_PATH/etc/fstab
         ;;
     btrfs) 
-        echo "$ROOT_PART /   $FSTYPE    ssd,discard,noatime  0   1" >> $ROOT_PATH/etc/fstab
+        echo "$ROOT_PART /   $FSTYPE    ssd,noatime  0   1" >> $ROOT_PATH/etc/fstab
         ;;
     *)
         echo "Rootfs using $FSTYPE, make sure it is a valid value."
