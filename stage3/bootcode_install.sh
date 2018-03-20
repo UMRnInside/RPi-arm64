0a128673035527e1135f6fd88ac7954aa9fb1bca
jesset <jesse.tian@me.com>
2018-03-20 19:30:12 +0800

    fix: VC userland building relative path problem

--- stage3/bootcode_install.sh
+++ stage3/bootcode_install.sh
@@ -44,19 +44,25 @@ fi
 
 if [ ${INSTALL_VC64:=0} -eq 1 ]; then
     echo "Installing VideoCore userland(/opt/vc) aarch64 binary..."
-    git clone --depth=1 https://github.com/raspberrypi/userland
+    ROOT_PATH=$(readlink -f ${ROOT_PATH}) # workaround, to get abs path
+    pushd $BUILD_PATH
+    test -d userland || git clone --depth=1 https://github.com/raspberrypi/userland
     rm -rf ./userland/build && mkdir -p ./userland/build
     pushd ./userland/build
     cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_BUILD_TYPE=release -DARM64=ON -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ -DCMAKE_ASM_COMPILER=aarch64-linux-gnu-gcc -DVIDEOCORE_BUILD_DIR=/opt/vc ../
     make -j $(nproc)
     make install DESTDIR=$ROOT_PATH/
+    popd
+    popd
 
     for _bin in ${ROOT_PATH}/opt/vc/bin/* ;do
         _binbase=$(basename $_bin)
-        echo "Symlinking /opt/vc/bin/${_binbase} ..."
-        cd ${ROOT_PATH}/usr/bin/ && ln -sf /opt/vc/bin/${_binbase} ./
+        if test -e ${_bin};then
+            cd ${ROOT_PATH}/usr/bin/ && \
+            ln -sf /opt/vc/bin/${_binbase} ./ && \
+            echo "Symlinked /opt/vc/bin/${_binbase} ..."
+        fi
     done
-    popd
 fi
 
 echo "Done."
