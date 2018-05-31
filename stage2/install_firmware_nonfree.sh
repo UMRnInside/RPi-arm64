1e1e3a20fe8322ecdc9ff4ba7c03676ad0b6e038
UMRnInside <newpage2@foxmail.com>
2018-05-31 13:29:21 +0800

    stage2: add install_firmware_nonfree.sh
    This new script fetches non-free firmware from
    RPi-distro/firmware-nonfree
    
    Now it should work

---
+++ stage2/install_firmware_nonfree.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. $(dirname $0)/../global_definitions
+
+mkdir -p $BUILD_PATH
+
+pushd $BUILD_PATH
+
+if [ $FETCH_METHOD == "git" ]; then
+    echo "Fetching firmware-nonfree using git clone ..."
+    git clone $GITCLONE_ARGS ${GIT_PROTOCOL}github.com/RPi-Distro/firmware-nonfree
+elif [ $FETCH_METHOD == "wget" ]; then
+    echo "Fetching formware-nonfree using wget ..."
+    incomplete=1
+    while [ $incomplete -ne 0 ]; 
+    do
+        rm firmware-nonfree.tar.gz
+        wget -O firmware-nonfree.tar.gz -c https://github.com/RPi-Distro/firmware-nonfree/archive/master.tar.gz
+        tar -xf firmware-nonfree.tar.gz
+        incomplete=$?
+    done
+    mv firmware-nonfree-master firmware-nonfree
+fi
+
+popd
+
+echo "Installing firmware..."
+cp -a $BUILD_PATH/firmware-nonfree/ $ROOT_PATH/lib/firmware
+
