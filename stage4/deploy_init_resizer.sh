795f88446c7f61469a710c45f44a188972078145
sherylynn <352281674@qq.com>
2018-09-26 00:44:11 +0800

    update Dockerfile and f2fs-tools from 1.10 to 1.11 in arm64

--- stage4/deploy_init_resizer.sh
+++ stage4/deploy_init_resizer.sh
@@ -51,10 +51,10 @@ fi
 
 # workaround for fsck.f2fs missing "-y" option
 #   Bugreport: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=883026
-#   Solution: upgrade to unstable v1.10
+#   Solution: upgrade to unstable v1.11
 if [ "$FSTYPE" = "f2fs" ];then
-    for pkg in libf2fs0_1.10.0-1_arm64.deb \
-               f2fs-tools_1.10.0-1_arm64.deb
+    for pkg in libf2fs0_1.11.0-1_arm64.deb \
+               f2fs-tools_1.11.0-1_arm64.deb
     do
         wget -c -P $ROOT_PATH/ "http://ftp.debian.org/debian/pool/main/f/f2fs-tools/${pkg}"
         chroot $ROOT_PATH dpkg -i /${pkg}
