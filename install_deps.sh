a00a147981fdd3347fcdfaae632c230d4cd64440
sherylynn <352281674@qq.com>
2018-09-25 23:29:10 +0800

    新增加了依赖安装脚

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 sudo apt install \
   wget busybox unzip tar patch parted \
   qemu-user-static debootstrap \
-  dosfstools btrfs-progs \
+  dosfstools btrfs-progs f2fs-tools\
   make build-essential bc xxd kmod vim cmake \
   gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
